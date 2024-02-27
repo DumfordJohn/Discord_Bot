cdd23f385e80b9396c9fddd8f36768379eddd2bd
dumforjh <91568702+DumfordJohn@users.noreply.github.com>
2024-02-27 11:59:06 -0500

    added the react command

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions] });
 
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
@@ -37,5 +37,7 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
+
+
 
 client.login(token);
